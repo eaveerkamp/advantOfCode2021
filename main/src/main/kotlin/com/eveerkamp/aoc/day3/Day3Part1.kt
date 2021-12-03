1bea11f0a28b76cc5367a5dac2af6a4050a4e10f
eveerkamp <eveerkamp@bol.com>
2021-12-03 11:02:27 +0100

    Day3

--- main/src/main/kotlin/com/eveerkamp/aoc/day3/Day3Part1.kt
+++ main/src/main/kotlin/com/eveerkamp/aoc/day3/Day3Part1.kt
@@ -1,12 +1,38 @@
-package com.eveerkamp.aoc.day2
+package com.eveerkamp.aoc.day3
 
 import com.eveerkamp.aoc.utils.readFile
+import java.lang.Integer.parseInt
 
 class Day3Part1 {
     private val input = readFile("day3/input.txt")
         .lines()
 
     fun calculate(): Int {
-        return 0
+        val bitsByPosition: MutableMap<Int, MutableList<Int>> = mutableMapOf()
+        input.map { bit ->
+            bit.forEachIndexed { position, char ->
+                bitsByPosition[position]?.add(char.digitToInt()) ?: bitsByPosition.put(
+                    position,
+                    mutableListOf(char.digitToInt())
+                )
+            }
+        }
+
+        val gammaBits: MutableList<String> = mutableListOf()
+        val epsilonsBits: MutableList<String> = mutableListOf()
+
+        bitsByPosition.toSortedMap().forEach { (_, bits) ->
+            if (bits.count { it == 1 } > bits.count { it == 0 }) {
+                gammaBits.add("1")
+                epsilonsBits.add("0")
+            } else if (bits.count { it == 0 } > bits.count { it == 1 }) {
+                gammaBits.add("0")
+                epsilonsBits.add("1")
+            }
+        }
+
+        val gamma = parseInt(gammaBits.joinToString(""), 2)
+        val epsilon = parseInt(epsilonsBits.joinToString(""), 2)
+        return gamma * epsilon
     }
 }
--- main/src/main/kotlin/com/eveerkamp/aoc/day3/Day3Part2.kt
+++ main/src/main/kotlin/com/eveerkamp/aoc/day3/Day3Part2.kt
@@ -1,12 +1,66 @@
-package com.eveerkamp.aoc.day2
+package com.eveerkamp.aoc.day3
 
 import com.eveerkamp.aoc.utils.readFile
+import java.lang.Integer.parseInt
 
 class Day3Part2 {
     private val input = readFile("day3/input.txt")
         .lines()
 
     fun calculate(): Int {
-        return 0
+       return getOutput(input, ::filterCO2Scrubber) * getOutput(input, ::filterOxygenGenerator)
+    }
+
+    private fun getOutput(input: List<String>, filter: (input: List<String>, position: Int) -> List<String>) : Int {
+        var copyInput = input
+        while (copyInput.size > 1) {
+            for(position in 0..copyInput.maxOf { it.length }){
+                if (copyInput.size > 1) {
+                    copyInput = filter(copyInput, position)
+                }
+            }
+        }
+        return parseInt(copyInput.first(), 2)
+    }
+
+    private fun filterOxygenGenerator(
+        input: List<String>,
+        position: Int
+    ): List<String> {
+        var oxygenInput = input
+        val bits = getBitsByPosition(oxygenInput)[position] ?: listOf()
+        oxygenInput = if (bits.count { it == 1 } >= bits.count { it == 0 }) {
+            oxygenInput.filter { it[position].toString() == "1" }
+        } else {
+            oxygenInput.filter { it[position].toString() == "0" }
+        }
+        return oxygenInput
+    }
+
+    private fun filterCO2Scrubber(
+        input: List<String>,
+        position: Int
+    ): List<String> {
+        var co2ScrubberInput = input
+        val bits = getBitsByPosition(co2ScrubberInput)[position] ?: listOf()
+        co2ScrubberInput = if (bits.count { it == 1 } >= bits.count { it == 0 }) {
+            co2ScrubberInput.filter { it[position].toString() == "0" }
+        } else {
+            co2ScrubberInput.filter { it[position].toString() == "1" }
+        }
+        return co2ScrubberInput
+    }
+
+    private fun getBitsByPosition(input: List<String>): MutableMap<Int, MutableList<Int>> {
+        val bitsByPosition: MutableMap<Int, MutableList<Int>> = mutableMapOf()
+        input.map { bit ->
+            bit.forEachIndexed { position, char ->
+                bitsByPosition[position]?.add(char.digitToInt()) ?: bitsByPosition.put(
+                    position,
+                    mutableListOf(char.digitToInt())
+                )
+            }
+        }
+        return bitsByPosition
     }
 }
