24c0dc64820f5afea9319706dc9a8a7dadb22c0a
eveerkamp <eveerkamp@bol.com>
2021-12-05 20:04:44 +0100

    Day5

--- main/src/main/kotlin/com/eveerkamp/aoc/AdventOfCode.kt
+++ main/src/main/kotlin/com/eveerkamp/aoc/AdventOfCode.kt
@@ -1,5 +1,6 @@
 package com.eveerkamp.aoc
 
+import com.eveerkamp.aoc.day5.Day5Part2
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +9,5 @@ class AdventOfCode
 
 fun main(args: Array<String>) {
     runApplication<AdventOfCode>(*args)
+    println("Result ${Day5Part2().calculate()}")
 }
